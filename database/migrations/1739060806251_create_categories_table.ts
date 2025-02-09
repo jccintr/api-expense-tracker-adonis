4318de137d80050205bc38141cbd718d1cef5c90
Julio Cesar <jccintr@gmail.com>
2025-02-08 21:29:12 -0300

    Criado categories migration

--- database/migrations/1739060806251_create_categories_table.ts
+++ database/migrations/1739060806251_create_categories_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'accounts'
+  protected tableName = 'categories'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
