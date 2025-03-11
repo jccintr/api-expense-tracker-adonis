fd0b04f009b5d29d9db8ba3c1f8d7c9b4cf4d833
Julio Cesar <jccintr@gmail.com>
2025-03-11 17:42:39 -0300

    corrigido erro TS em getWeekNumber

--- app/controllers/transactions_controller.ts
+++ app/controllers/transactions_controller.ts
@@ -428,9 +428,9 @@ export default class TransactionsController {
     };
 }
 
-getWeekNumber(date) {
+getWeekNumber(date: Date) : number {
   const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
-  const pastDays = (date - firstDayOfYear) / (24 * 60 * 60 * 1000);
+  const pastDays = (date.getTime() - firstDayOfYear.getTime()) / (24 * 60 * 60 * 1000);
   return Math.ceil((pastDays + firstDayOfYear.getDay() + 1) / 7);
 }
 
